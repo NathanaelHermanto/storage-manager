fe410df64513f9743db23d786a7ec1a59859df34
Nathanael Hermanto <nathanael887@gmail.com>
2022-07-12 21:27:42 +0200

    sadkajdskasjldasjlkasdkndsak

--- index.js
+++ index.js
@@ -18,6 +18,4 @@ app.use('/api/items', require('./routes/api/items.js'));
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-    throw new Error("Fuck my life")
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
